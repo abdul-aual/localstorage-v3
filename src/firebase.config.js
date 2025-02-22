3a1cba69939d71f82fdd2ad62f0a7fb0d603877a
abdul-aual <mdabdulaual38@gmail.com>
2025-02-22 16:02:46 +0600

    hello

--- src/firebase.config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAXuSroKimG74nH0B5WFIw58eTj-8pT9QM",
-    authDomain: "localstorage-v3.firebaseapp.com",
-    projectId: "localstorage-v3",
-    storageBucket: "localstorage-v3.firebasestorage.app",
-    messagingSenderId: "240212519294",
-    appId: "1:240212519294:web:73da33fe29d6e66798eb2f"
-  };
-  export default firebaseConfig;
